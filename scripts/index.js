01ef77cfdfe14a0ce4dd7dc40eae64198e85c30b
Karim Wiener <karimwiener@gmail.com>
2026-01-13 06:13:44 -0600

    Final stage Spot project sprint4

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,27 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileInput = editProfileModal.querySelector("#profile-name");
+const editDescriptionInput = editProfileModal.querySelector(
+  "#profile-description"
+);
+
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const addCardForm = newPostModal.querySelector(".modal__form");
+const linkInputElement = newPostModal.querySelector("#image-input");
+const captionInputElement = newPostModal.querySelector("#caption-description");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
 
 editProfileButton.addEventListener("click", function () {
+  editProfileInput.value = profileNameElement.textContent;
+  editDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +38,20 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileInput.value;
+  profileDescriptionElement.textContent = editDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(linkInputElement.value);
+  console.log(captionInputElement.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+addCardForm.addEventListener("submit", handleAddCardSubmit);
