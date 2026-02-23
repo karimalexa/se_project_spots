5c69e1c13780f983fd1419b153eb371832ac9959
Karim Wiener <karimwiener@gmail.com>
2026-02-23 03:15:57 -0600

    Spot stage 6 Array and Loop forEach

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,39 @@
+const inicialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
-  ".modal__close-button"
+  ".modal__close-button",
 );
 const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileInput = editProfileModal.querySelector("#profile-name");
 const editDescriptionInput = editProfileModal.querySelector(
-  "#profile-description"
+  "#profile-description",
 );
 
 const newPostButton = document.querySelector(".profile__add-button");
@@ -18,7 +45,7 @@ const captionInputElement = newPostModal.querySelector("#caption-description");
 
 const profileNameElement = document.querySelector(".profile__name");
 const profileDescriptionElement = document.querySelector(
-  ".profile__description"
+  ".profile__description",
 );
 
 function openModal(modal) {
@@ -63,3 +90,8 @@ function handleAddCardSubmit(evt) {
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 addCardForm.addEventListener("submit", handleAddCardSubmit);
+
+inicialCards.forEach(function (item) {
+  console.log(item.name);
+  console.log(item.link);
+});
